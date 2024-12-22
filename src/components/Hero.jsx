506dd89f503d0f936896ff9d828ec3a660760125
ShikharJoshi98 <shikharjoshi89@gmail.com>
2024-12-23 02:00:58 +0530

    finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import { Button } from './ui/button'
-
-
+import resume from '../../public/Shikhar_Joshi_resume.pdf'
 
 const Hero = () => {
   return (
@@ -20,7 +19,7 @@ const Hero = () => {
           <motion.div whileInView={{ opacity: 1, y: 0 }}
                         initial={{ opacity: 0, y: 6 }}
                         transition={{duration: 0.5}}>
-            <Button className=' mb-8 md:mb-0 w-full sm:w-fit border-t-2 rounded-full border-none bg-[#3b3b3b] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 hover:text-black duration-500'><a href="#" download className='bg-clip-text  ' >Download Resume</a></Button>
+            <Button className=' mb-8 md:mb-0 w-full sm:w-fit border-t-2 rounded-full border-none bg-[#3b3b3b] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 hover:text-black duration-500'><a href={resume} download className='bg-clip-text  ' >Download Resume</a></Button>
             </motion.div>
          </div>
       </div>  

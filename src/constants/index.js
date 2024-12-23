da57b1c7bd8326f38bfa8d6029897f221e5207d9
ShikharJoshi98 <shikharjoshi89@gmail.com>
2024-12-24 01:08:50 +0530

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,7 +10,7 @@ export const EXPERIENCES = [
     role: "Course Tutor",
     company: "Course Hero",
     description: `Utilizing my deep understanding of Computer Science principles, I created comprehensive and step-by-step solutions to a wide range of academic problems.`,
-    technologies: ["DSA", "C++", "JavaScript", "Python","Java","HTML","CSS",],
+    technologies: ["Data Structures & Algorithm", "C++", "JavaScript", "Python","Java","HTML","CSS",],
   }
 ];
 
